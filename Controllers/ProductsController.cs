b7d8ceadb1703824735069160039615393e8e31d
David Ramos Penott <davepenott@protonmail.com>
2018-06-05 01:31:16 -0300

    New View and Controller Products

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BlogNet.Controllers 
+{ 
+    public class ProductsController : Controller 
+    { 
+        // 
+        // GET: /HelloWorld/ 
+ 
+        public string Index() 
+        { 
+            return "This is my <b>default</b> action..."; 
+        } 
+ 
+        // 
+        // GET: /HelloWorld/Welcome/ 
+ 
+        public string Welcome() 
+        { 
+            return "This is the Welcome action method..."; 
+        } 
+    } 
+}
